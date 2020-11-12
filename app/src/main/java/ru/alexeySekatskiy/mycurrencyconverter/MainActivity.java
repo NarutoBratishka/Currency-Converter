539c89d6d2d49aea7690fb364719b6c1a118a8c0
NarutoBratishka <katorabian@gmail.com>
2020-11-12 09:02:35 +0700

    fix sum view at editviews

--- app/src/main/java/ru/alexeySekatskiy/mycurrencyconverter/MainActivity.java
+++ app/src/main/java/ru/alexeySekatskiy/mycurrencyconverter/MainActivity.java
@@ -1,7 +1,12 @@
 package ru.alexeySekatskiy.mycurrencyconverter;
 
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startEditPreferences();
     }
+
+    public void startEditPreferences() {
+        EditText firstMoneyText = (EditText)findViewById(R.id.first_money_text);
+        firstMoneyText.setText("" + 120.5);
+        EditText secondMoneyText = (EditText)findViewById(R.id.second_money_text);
+        secondMoneyText.setText("" + 2.2);
+    }
+
 }
